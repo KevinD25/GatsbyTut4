936613efb8ab9057595fcb5b053a5dc1b9533f6f
Scazi <kevin-davis@hotmail.com>
2020-07-17 21:59:14 +0200

    Project up to date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,71 @@
 import React from "react"
+import Layout from "../components/layout"
+import { graphql, Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+import { css } from "@emotion/core"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <div>
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1 px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.date}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  - {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields{
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/newborn-panda.md
@@ -0,0 +1,8 @@
+---
+title: "Pandas and baby's"
+date: "2016-07-20"
+---
+
+After precautionary checks the new born panda is reunited with mum.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2MC-ASv4YAQ" frameborder="0" allowfullscreen></iframe>
